95e5c53470b85820baf0b217ee5f2ccc9c0c0aeb
kiddyyhhygithub <kiddyflash@163.com>
2019-06-20 17:37:52 +0800

    20190620

---
+++ Bussiness/IOT.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2019/6/20 0020 下午 5:08
+# @Author  : Richard
+# @Site    : 
+# @File    : IOT.py
+# @email   : kiddyflash@163.com
+
+class IOT():
+    def __init__(self,baseUrl):
+        self._baseUrl = ''
+        pass
+
+    def cmd_iot(self,value):
+        '''
+        下发指令
+        :param value:
+        :return:
+        '''
+
+
+        return
